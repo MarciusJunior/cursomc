52db57aeb20697c21b9bd2a95179e94795f8e127
Marcius Junior <marcius.junior@hotmail.com>
2021-10-06 11:44:47 -0300

    PUT, DELETE de Categoria, outros pequenos ajustes

--- src/main/java/com/java/cursomc/domain/Categoria.java
+++ src/main/java/com/java/cursomc/domain/Categoria.java
@@ -3,7 +3,6 @@ package com.java.cursomc.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -22,48 +21,41 @@ public class Categoria implements Serializable{
 	private String nome;
 	
 	
-	@ManyToMany(mappedBy = "categorias")
+	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
-	
-	
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
 
 	public Categoria() {
 	}
-
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -73,9 +65,12 @@ public class Categoria implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 
 }
--- src/main/java/com/java/cursomc/services/CategoriaService.java
+++ src/main/java/com/java/cursomc/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.java.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.java.cursomc.domain.Categoria;
 import com.java.cursomc.repositories.CategoriaRepository;
+import com.java.cursomc.services.exceptions.DataIntegrityException;
 import com.java.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,20 +18,33 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
-		
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException
-				("Objeto não encontrado! Id: "+ id 
-						+ ", Tipo: " + Categoria.class.getName()));
-
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
 
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
