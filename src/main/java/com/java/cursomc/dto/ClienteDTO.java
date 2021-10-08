42b76a6b32e4710eca8a4e18be83d00e3166b6e9
Marcius Junior <marcius.junior@hotmail.com>
2021-10-08 11:36:49 -0300

    Validação Customizada: Email não repetido na atualização de Cliente

--- src/main/java/com/java/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/java/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.java.cursomc.domain.Cliente;
+import com.java.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
