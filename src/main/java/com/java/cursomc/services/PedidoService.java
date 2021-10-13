3f4137da94046c2e736492884ec757a47c6c3a6c
Marcius Junior <marcius.junior@hotmail.com>
2021-10-13 10:54:56 -0300

    Implementando toString de Pedido

--- src/main/java/com/java/cursomc/services/PedidoService.java
+++ src/main/java/com/java/cursomc/services/PedidoService.java
@@ -59,7 +59,8 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
